58260ed7e87c366d933d2e01229e09f1e12f5c03
Carrie Jones <c.jessica.jones@gmail.com>
2021-02-07 22:16:16 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -42,6 +42,8 @@ class Scraper
       profile_quote = data.css(".vitals-text-container").css(".profile-quote").text
       bio = data.css(".details-container").css(".bio-block details-block").css(".bio-content content-holder").css(".title-holder").css(".description-holder").css('p').text
       student_hash = {:twitter=>twitter, :linkedin=>linkedin, :github=>github, :blog=>blog, :profile_quote=>profile_quote, :bio=>bio}
+      binding-pry
+      
       student_hash
       end
     end
