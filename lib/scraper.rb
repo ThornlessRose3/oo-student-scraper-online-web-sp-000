b998f36e1cd36103d68402c7c3449e70d776fb4e
Carrie Jones <c.jessica.jones@gmail.com>
2021-02-07 21:19:05 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -25,7 +25,7 @@ class Scraper
       github = data.
       # blog = data. ???
       profile_quote = data.css(".vitals-text-container").css(".profile-quote").text
-      bio = data.css(."details-container").css(."bio-block details-block").css(".bio-content content-holder").css(".title-holder").css(".description-holder").css('p').text
+      bio = data.css(".details-container").css(".bio-block details-block").css(".bio-content content-holder").css(".title-holder").css(".description-holder").css('p').text
       s = {:twitter=>twitter, :linkedin=>linkedin, :github=>github, :blog=>blog, :profile_quote=>profile_quote, :bio=>bio}
   end
   s
